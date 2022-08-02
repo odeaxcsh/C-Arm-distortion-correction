7f734387b8411f833256db101294e04b3a568910
Abolfazl <odaat.iaath@gmail.com>
2022-08-03 02:20:54 +0430

    update base algorithm to detect outliers - WIP

--- c_guide.py
+++ c_guide.py
@@ -1,33 +1,28 @@
+from collections import namedtuple
 import pandas as pd
 import matplotlib.pyplot as plt
 import numpy as np
+
+error1 = 25
+error2 = 25
+
 
 df_org = pd.read_csv('sample.csv')
 
 original_x, original_y = df_org.iloc[:, 0], df_org.iloc[:, 1]
 original_points = np.array([original_x, original_y]).T
 
-error1 = 25
-error2 = 25
 
 detected_points = np.random.permutation(original_points)[:50] + np.random.uniform(-10, 10, (50, 2))
-
 
 np.random.shuffle(detected_points)
 np.random.shuffle(original_points)
 
-
-# original_points = original_points[:100, :]
+original_points = original_points[:100, :]
 original_x, original_y = original_points[:, 0], original_points[:, 1]
 
 detected_x, detected_y = detected_points[:, 0], detected_points[:, 1]
 
-
-# Find nearest point of original_points for each point in detected_points
-with open('C-Guide_correspondance.txt', 'w') as f: 
-    for i, p in enumerate(detected_points):
-        j = np.argmin(np.linalg.norm(p - original_points, axis=1))
-        f.write(str(i) + ' ' + str(j) + '\n')
 
 
 
@@ -37,91 +32,103 @@ print(f'''
     ratio of detected points: {len(detected_points) / len(original_points)}
 ''')
 
+outliers = []
+for i, p in enumerate(detected_points):
+    dist = np.min(np.linalg.norm(p - original_points, axis=1))
+    if dist > error1:
+        print(f'Outlier detected at {i} with distance {dist}')
+        outliers.append(i)
 
-# set plot X, Y limits
 plt.xlim(0, max(original_x))
 plt.ylim(0, max(original_y))
 plt.plot(original_x, original_y, 'bo', label='original')
 plt.plot(detected_x, detected_y, 'ro', label='detected')
+plt.plot(detected_points[outliers, 0], detected_points[outliers, 1], 'gx', label='outliers')
 plt.show()
 
 
 def getDistanceMatrix(points):
     return np.array([np.linalg.norm(p - points, axis=1) for p in points])
 
-
 original_distance_matrix = getDistanceMatrix(original_points)
 detected_distance_matrix = getDistanceMatrix(detected_points)
 
+n, m = len(detected_points), len(original_points)
 
+Match = namedtuple('Match', ['original', 'detected', 'size'])
 
-
-d = {}
-for i in range(len(detected_points)):
-    d[i] = []
-    for j in range(len(original_points)):
-        d[i].append(j)
-print(sum([len(v) for v in d.values()]))
 
 matches = []
-for i in d[0]:
-    for j in d[1]:
+for i in range(m):
+    for j in range(m):
         if np.linalg.norm(detected_distance_matrix[0, 1] - original_distance_matrix[i, j]) < error1:
-            matches.append((i, j))
+            matches.append(Match(original=(i, j), detected=(0, 1), size=2))
 print(len(matches))
 
 
-def isOkay(matches, new, step, original_distance_matrix, detected_distance_matrix):
-    for i, j in enumerate(matches):
+def isOkay(match, new, step, original_distance_matrix, detected_distance_matrix):
+    for i, j in zip(match.detected, match.original):
         if np.linalg.norm(detected_distance_matrix[i, step] - original_distance_matrix[j, new]) > error2:
             return False
     return True
 
 
-candidates = [len(matches)]
+candidatesInEachStep = [len(matches)]
+
+print('Number of matches in each step:')
+print('\t 1. ', len(matches))
 
 for step in range(2, len(detected_points)):
     new_matches = []
-    print(f'Start Step {step}')
     for match in matches:
         found = False
-        for i in d[step]:
+        for i in range(m):
             if isOkay(match, i, step, original_distance_matrix, detected_distance_matrix):
-                new_matches.append((*match, i))
-                found = True
-                # print(*match, i)
-        
-    matches = new_matches
-    print(f'{step} {len(matches)}')
-    candidates.append(len(matches))
+                new_matches.append(Match(
+                    original=(*match.original, i),
+                    detected=(*match.detected, step),
+                    size=match.size + 1
+                ))
+                found = True    
+    if len(new_matches) > 0:
+        matches = new_matches
+
+    ###########################
+    # containing_outlier = [match for match in matches if match.detected[-1] == step - 2]
+
+    # matches = [match for match in matches if match.detected[-1] > step - 2]
+    ###########################
     
-    draw_matches = [match for match in matches if len(match) > step + 1 - int(step / 3)]
-    for match in draw_matches:
-        plt.clf()
+    print(f'\t {step}. ', len(matches))
+    candidatesInEachStep.append(len(matches))
 
-        for i in range(step + 1):
-            plt.plot(detected_points[i, 0], detected_points[i, 1], 'rx')
-        plt.plot(detected_points[step + 1, 0], detected_points[step + 1, 1], 'yx')
+    # draw_matches = matches
+    # for match in draw_matches:
+    #     plt.clf()
+
+    #     for i in range(step + 1):
+    #         plt.plot(detected_points[i, 0], detected_points[i, 1], 'rx')
+    #     plt.plot(detected_points[step + 1, 0], detected_points[step + 1, 1], 'yx')
 
 
-        for i in d[step + 1]:
-            plt.plot(original_points[i, 0], original_points[i, 1], 'go')
+    #     for i in d[step + 1]:
+    #         plt.plot(original_points[i, 0], original_points[i, 1], 'go')
         
-        x = [original_points[i, 0] for i in match] + [original_points[match[0], 0]]
-        y = [original_points[i, 1] for i in match] + [original_points[match[0], 1]]
+    #     x = [original_points[i, 0] for i in match] + [original_points[match[0], 0]]
+    #     y = [original_points[i, 1] for i in match] + [original_points[match[0], 1]]
 
-        plt.plot(x, y, 'b-')
+    #     plt.plot(x, y, 'b-')
 
-        plt.xlim(0, max(original_x))
-        plt.ylim(0, max(original_y))
-        plt.pause(1 / len(matches))
-    plt.pause(0.1)
+    #     plt.xlim(0, max(original_x))
+    #     plt.ylim(0, max(original_y))
+    #     plt.pause(1 / len(matches))
+    # plt.pause(0.1)
 
 
-print(len(match))
-with open('C-Guide_correspondance-match.txt', 'w') as f:
-    for i, m in enumerate(match):
-        f.write(str(i) + ' ' + str(m) + '\n')
-    
-plt.plot(list(range(len(candidates))), candidates, )
+print()
+print(f'Number of matches: {len(matches)}')
+if len(matches) != 0:
+    print(f'Biggest candidate: {max(match.size for match in matches)}')
+
+plt.plot(list(range(len(candidatesInEachStep))), candidatesInEachStep)
 plt.show()
